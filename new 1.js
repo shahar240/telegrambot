a5ef039b0acee517f25bb55e14926a9a83ace775
shahar shavit <shahar240@gmail.com>
2019-03-13 14:49:43 +0200

    added jwt genereator for git

---
+++ new 1.js
@@ -0,0 +1,24 @@
+{
+	"code": "EREQUEST",
+	"number": 2627,
+	"lineNumber": 17,
+	"state": 1,
+	"class": 14,
+	"serverName": "EC2AMAZ-UPVTS3A",
+	"procName": "CreateUser",
+	"originalError": {
+		"info": {
+			"number": 2627,
+			"state": 1,
+			"class": 14,
+			"message": "Violation of UNIQUE KEY constraint 'Unique_Email_Users'. Cannot insert duplicate key in object 'dbo.Users'. The duplicate key value is (shahar240@gmail.com).",
+			"serverName": "EC2AMAZ-UPVTS3A",
+			"procName": "CreateUser",
+			"lineNumber": 17,
+			"name": "ERROR",
+			"event": "errorMessage"
+		}
+	},
+	"name": "RequestError",
+	"precedingErrors": []
+}
---
+++ telegramBot/telegramBot-BL/Helpers/JWT.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+using System.IO;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+using System.Security.Claims;
+using System.Configuration;
+
+namespace telegramBot_BL.Helpers
+{
+    public static class JWTs
+    {
+        static string Create(object payload, string key)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            
+           
+            //create jwt
+            SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor
+            {
+                Audience = ConfigurationManager.AppSettings["Audience"],
+                Expires = DateTime.UtcNow.AddMinutes(10),
+                Subject = new ClaimsIdentity(ObjectToClaims(payload)),
+                Issuer = ConfigurationManager.AppSettings["Issuer"],
+                SigningCredentials = new SigningCredentials(getKey(key), SecurityAlgorithms.RsaSha256, SecurityAlgorithms.Sha512Digest)
+
+            };
+            var token = handler.CreateToken(descriptor);
+
+            return "";
+        }
+
+        private static RsaSecurityKey getKey(string keyString)
+        {
+            var keyBytes = Convert.FromBase64String(keyString); // your key here
+            AsymmetricKeyParameter asymmetricKeyParameter = PublicKeyFactory.CreateKey(keyBytes);
+            RsaKeyParameters rsaKeyParameters = (RsaKeyParameters)asymmetricKeyParameter;
+            RSAParameters rsaParams = new RSAParameters
+            {
+                Modulus = rsaKeyParameters.Modulus.ToByteArrayUnsigned(),
+                Exponent = rsaKeyParameters.Exponent.ToByteArrayUnsigned()
+            };
+            return new RsaSecurityKey(rsaParams);
+        }
+
+        private static List<Claim> ObjectToClaims(object obj)
+        {
+            List<Claim> lst = new List<Claim>();
+            var props = obj.GetType().GetProperties();
+            foreach(var prop in props)
+            {
+                lst.Add(new Claim(prop.Name, prop.GetValue(obj).ToString()));
+            }
+            return lst;
+        }
+    }
+}
