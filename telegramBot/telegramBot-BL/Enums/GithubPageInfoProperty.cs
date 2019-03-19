18ef57d20dc7f15b4851501c715912a2a3bc7891
shahar shavit <shahar240@gmail.com>
2019-03-19 11:55:06 +0200

    built basic github v4 api serch query builder

---
+++ telegramBot/telegramBot-BL/Enums/GithubPageInfoProperty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telegramBot_BL.Enums
+{
+    internal enum GithubPageInfoProperty
+    {
+        startCursor,
+        endCursor,
+        hasNextPage,
+        hasPreviousPage
+    }
+}
