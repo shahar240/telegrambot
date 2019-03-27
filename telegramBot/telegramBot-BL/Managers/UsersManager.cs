3c3067cd2b1220c094a0535ff7975a39e9c7d949
shahar shavit <shahar240@gmail.com>
2019-03-27 16:03:46 +0200

    added git update registration options

---
+++ telegramBot/telegramBot-BL/Managers/UsersManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using telegramBot_Common;
+using telegramBot_DAL.Repositories.MsSql;
+
+namespace telegramBot_BL.Managers
+{
+    public class UsersManager
+    {
+        private SqlUsersRepository _usersRepo;
+
+        public UsersManager()
+        {
+            _usersRepo = new SqlUsersRepository();
+        }
+
+        public async Task<bool> EnableGitHub(int telgramId, string gitToken)
+        {
+            User user = new User()
+            {
+                gitToken = gitToken,
+                Id =telgramId
+            };
+            return await _usersRepo.UpdateOrCreate(user);
+        }
+
+        public async Task<bool> DisableGitHub(int telgramId)
+        {
+            User user = new User()
+            {
+                gitToken = "",
+                Id = telgramId
+            };
+            return await _usersRepo.UpdateOrCreate(user);
+        }
+
+        public async Task<bool> SetUpdateFrequency(int telgramId, int frequency)
+        {
+            User user = new User()
+            {
+                updateFrequency = frequency,
+                Id = telgramId
+            };
+            return await _usersRepo.UpdateOrCreate(user);
+        }
+    }
+}
--- telegramBot/telegramBot/Controllers/WebHookController.cs
+++ telegramBot/telegramBot/Controllers/WebHookController.cs
@@ -8,40 +8,67 @@ using System.Web.Http;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using telegramBot_BL.Helpers;
+using telegramBot_BL.Managers;
 
 namespace telegramBot.Controllers
 {
     public class WebHookController : ApiController
     {
+        private UsersManager _manager;
+
+        public WebHookController()
+        {
+            _manager = new UsersManager();
+        }
 
         public async Task<IHttpActionResult> Post(Update update)
         {
             var message = update.Message;
-            
-            Console.WriteLine("Received Message from {0}", message.Chat.Id);
-
             if (message.Type == MessageType.Text)
             {
+                if(message.Text[0]=='/')
+                {
+                    string[] cmd = message.Text.Split(' ');
+                    switch(cmd[0])
+                    {
+                        case "/EnableGithub":
+                            await EnableGithub(message, cmd);
+                            break;
+                        case "/DisableGithub":
+                            await DisableGithub(message,cmd);
+                            break;
+                    }
+                }
                 // Echo each Message
                 await TelegramBot.Api.SendTextMessageAsync(message.Chat.Id, message.Text);
             }
-            else if (message.Type == MessageType.Photo)
-            {
-                // Download Photo
-                //var file = await Bot.Api.GetFileAsync(message.Photo.LastOrDefault()?.FileId);
-
-                //var filename = file.FileId + "." + file.FilePath.Split('.').Last();
-
-                //using (var saveImageStream = File.Open(filename, FileMode.Create))
-                //{
-                   // await Bot.Api.DownloadFileAsync(file.FilePath, saveImageStream);
-                //}
-
-                //await Bot.Api.SendTextMessageAsync(message.Chat.Id, "Thx for the Pics");
-            }
-
             return Ok();
         }
 
+        private async Task EnableGithub(Message message, string[] cmd)
+        {
+            if (cmd.Length != 2)
+                await TelegramBot.Api.SendTextMessageAsync(message.Chat.Id, "invalid command");
+            else
+            {
+                if (await _manager.EnableGitHub(message.From.Id, cmd[1]))
+                    await TelegramBot.Api.SendTextMessageAsync(message.Chat.Id, "you will now recive github updates");
+                else
+                    await TelegramBot.Api.SendTextMessageAsync(message.Chat.Id, "registration failed. please try agia later");
+            }
+        }
+
+        private async Task DisableGithub(Message message, string[] cmd)
+        {
+            if (cmd.Length != 1)
+                await TelegramBot.Api.SendTextMessageAsync(message.Chat.Id, "invalid command");
+            else
+            {
+                if (await _manager.EnableGitHub(message.From.Id, cmd[1]))
+                    await TelegramBot.Api.SendTextMessageAsync(message.Chat.Id, "you will now recive github updates");
+                else
+                    await TelegramBot.Api.SendTextMessageAsync(message.Chat.Id, "registration failed. please try agia later");
+            }
+        }
     }
 }
